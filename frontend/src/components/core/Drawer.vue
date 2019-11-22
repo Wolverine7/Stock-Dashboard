534ff08e5610942084e1a94b75dfb14d33ea98d5
Bobodzhon Isamov <bisamov@gmail.com>
2019-11-21 22:49:00 -0600

    Added the left navbar and top navbar

---
+++ frontend/src/components/core/Drawer.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-navigation-drawer id="app-drawer" v-model="inputValue" app dark floating persistent mobile-break-point="991" width="260">
+
+    </v-navigation-drawer>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
